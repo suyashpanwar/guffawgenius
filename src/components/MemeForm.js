91f57f889143e69b66a487201b64c5bf6e79a50a
suyashpanwar <suyashp124@gmail.com>
2024-02-12 22:54:02 +0530

    added images source file

--- src/components/MemeForm.js
+++ src/components/MemeForm.js
@@ -1,14 +1,21 @@
+import memesData from "./memesData"
+
 export default function MemeForm(){
+    function memeImage(){
+        const memeArray = memesData.data.memes;
+        const num = Math.floor(Math.random() * memeArray.length)
+        const url = memeArray[num].url;
+    }
     return(
-        <form className="meme-form">
+        <div className="meme-form">
             <div className="meme-form-input">
-                <input className="input-1" type="text" placeholder="Enter meme text" />
-                <input className="input-2" type="text" placeholder="Enter meme text" />
+                <input className="input-1" type="text" placeholder="TOP TEXT" />
+                <input className="input-2" type="text" placeholder="BOTTOM TEXT" />
             </div>
             <div className="meme-form-button">
-                <button className="meme-form-button-1" type="button">Get new meme image</button>
+                <button className="meme-form-button-1" type="button" onClick={memeImage}>Get new meme image</button>
                 <button className="meme-form-button-2" type="button">Generate meme</button>
             </div>
-        </form>
+        </div>
     )
 }
